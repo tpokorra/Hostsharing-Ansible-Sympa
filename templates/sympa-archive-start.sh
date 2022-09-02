b8668a077f00ec7f2610ba4c217df93df64a37c3
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-09-02 10:35:47 +0200

    fix for PIDs

--- templates/sympa-archive-start.sh
+++ templates/sympa-archive-start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export HOME=/home/pacs/{{pac}}/users/{{user}}
-export PID=$HOME/var/run/archived.pid
 export PERL5LIB=$HOME/perl5/lib/perl5/
 
 $HOME/sympa/sbin/archived.pl 2>&1 &
-echo $! > $PID
